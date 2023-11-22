8242be52113d1cfe10308ed8bf311247e248aa20
sauliuskarlinskas <127085630+sauliuskarlinskas@users.noreply.github.com>
2023-11-22 20:05:56 +0200

    11.22

---
+++ js/pages/home.js
@@ -0,0 +1,37 @@
+//IMPORTS
+
+//EXECUTIONS
+
+/* Header:start */
+/* Header:end */
+
+/* Hero:start */
+/* Hero:end */
+
+
+/* About me:start */
+/* About me:end */
+
+/* My services:start */
+/* My services:end */
+
+/* My resume:start */
+/* My resume:end */
+
+/* Freelance:start */
+/* Freelance:end */
+
+/* My portfolio:start */
+/* My portfolio:end */
+
+/* Founders:start */
+/* Founders:end */
+
+/* My blogs:start */
+/* My blogs:end */
+
+/* Contact me:start */
+/* Contact me:end */
+
+/* Footer:start */
+/* Footer me:end */
